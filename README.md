0411c3224331e46926dbadfa5885d4b9d7546e84
Mauricio M Marques <mauricio.marques@concrete.com.br>
2019-12-09 22:23:07 -0300

    Add Readme description Add step comments to the action .rb file

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# config_match_repo
+## Using the `config_match_repo` action for the first time
+
+1. Config a `Matchfile` in the `fatlane`:
+* Setup the `git_url` variable with your future certificates repo url direction.
+* Setup the `app_identifier` with your application(s) bundle ID(s).
+* Setup the environment variable `MATCH_PASSWORD` with a your choice value to be used as your repo criptography password
+2. First, create a `actions` folder in your `fastlane` folder and paste the `config_match_repo.rb` inside it.
+3. Run the action `fastlane run config_match_repo`
+
+#### At the moment you run the action, the following steps will proceed:
+1. The action will access your repo reading the configurations from your `Matchfile`.
+2. It will download and decrypt the repo content.
+3. Then a `Finder` window will be opened in a decripted repo copied folder.
+4. At this moment you will paste your certificates and mobile provisons inside this folder using the same structure descriobed in the Folder Structure section from match [documentation](https://docs.fastlane.tools/actions/match/).
+5. After pasting them, all you need to do is go back to the terminal and press any key.
+6. The folder will be encrypted again and the changes will be pushed to the repo.
+
+* After that, you can use all match commands normally. Like
+
+```
+match(
+      type: "development",
+      readonly: true,
+      verbose: true
+)
+```
+
+or 
+
+```
+match(
+      type: "appstore",
+      readonly: true,
+      verbose: true
+)
+```
+
+to sync your project with the configured certificates and mobile provisions
+
+## Updating certificates and mobile provisions repo with new ones
+
+To update configured certificates and mobile provisions in your repo, you just need to follow the same steps described above, using `Finder` to replace the current files in the repo for the new ones.
--- config_match_repo.rb
+++ config_match_repo.rb
@@ -6,32 +6,42 @@ module Fastlane
     module Actions
       class ConfigMatchRepoAction < Action
         def self.run(params)
+            # Load and read the Matchfile
             params = FastlaneCore::Configuration.create(Match::Options.available_options, Hash.new)
             params.load_configuration_file("Matchfile")
 
+            # Read repo url from the git_url variable configured in the Machfile
             git_url = params[:git_url]
             shallow_clone = false
             branch = 'master'
 
+            # Access and download the repo content
             storage = Match::Storage.for_mode('git', { git_url: git_url, shallow_clone: shallow_clone, git_branch: branch, clone_branch_directly: false})
             storage.download
 
+            # Decrypt the repo folder content
             encryption = Match::Encryption.for_storage_mode('git', { git_url: git_url, working_directory: storage.working_directory})
             encryption.decrypt_files
 
+            # Save the folder temporally in a working directory
             repo_dir = storage.working_directory
 
+            # Open folder in Finder
             system('open', repo_dir)
 
+            # Create, if needed, a .gitignore file, to avoid the *.DS_Store repo push
             File.open(File.join(repo_dir, '.gitignore'), "w+") { |file| file.write("*.DS_Store") }
 
             UI.important "Configure the repository path with the certificates and profile as indicated in the Folder Structure section of the Match documentation https://docs.fastlane.tools/actions/match/ ..."
             UI.important "After configuring the repository folder press any key"                                                                                                    
+            # Wait the user press any key
             STDIN.getch                                                                                                              
             UI.message "Encrypting repository folder..."
 
+            # Encrypt folder content
             encryption.encrypt_files
 
+            # Push folder to the repo
             storage.save_changes!(files_to_commit: ["."])
         end
       end
